5e0437a610229ef0703f190c3834b1d0c0fd2bb2
Andreas Binnewies <abinnewies@gmail.com>
2011-01-06 17:34:44 -0800

    Remove the validate fields.

--- demo/blog/forms.js
+++ demo/blog/forms.js
@@ -10,16 +10,10 @@ exports.LoginForm = forms.Form.extend({
 
 exports.RegisterForm = forms.Form.extend({
 	username: new forms.CharField(),
-
 	email: new forms.EmailField(),
-	verifyEmail: new forms.EmailField(),
-
 	password: new forms.CharField({
 			widget: new forms.widgets.PasswordInput()
-		}),
-	verifyPassword: new forms.CharField({
-			widget: new forms.widgets.PasswordInput()
-		}),
+		})
 });
 
 exports.CreateBlogForm = forms.Form.extend({
